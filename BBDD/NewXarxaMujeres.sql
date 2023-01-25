a6228b12ce1bfc3cb0768dbaa530f2d1e805dd89
V1CTOR-CLOUD <victorbg189@gmail.com>
2023-01-25 12:50:46 +0100

    Uploading new SQL

---
+++ BBDD/NewXarxaMujeres.sql
@@ -0,0 +1,71 @@
+#Creo la base de datos
+DROP DATABASE IF EXISTS XARXAMUJERES;
+CREATE DATABASE XARXAMUJERES;
+USE XARXAMUJERES;
+#Creo las tablas
+
+create table `Associated`(
+	`Date` datetime not null,
+    `Name` varchar(45) not null,
+    `Photo` mediumblob null,
+    `PC` integer,
+    `Loc`varchar(45),
+    `Prov`varchar(45),
+    `ID`varchar(45) not null,
+    `BirthDate`date not null,
+    `DisabDeg`integer not null,
+    `DisabType`varchar(45) not null,
+    `Phone`varchar(12) null,
+    `Mail`varchar(50) null,
+    primary key(`ID`)
+    );
+
+    create table `Employee`(
+	`Name` varchar(45) not null,
+    `Password` varchar(45) not null,
+    `Address` varchar(45) not null,
+    `BirthDate` datetime not null,
+    `Profile/Position` varchar(45) not null,
+    `Photo` mediumblob not null,
+    `DL` varchar(45) not null,
+    `PersonalVehicle` varchar(45) not null,
+    `SelfEmployedDisplacement` boolean not null,
+    `Adaptations` mediumblob not null,
+    `Loc`varchar(45),
+    `Prov`varchar(45),
+    `ID`varchar(45) not null,
+	primary key(`ID`)
+);
+
+create table `Session`(
+	`File` mediumblob not null,
+    `Type` varchar(45),
+    `StartDate` datetime not null,
+    `EndDate` datetime not null,
+    `Author` varchar(45) not null,
+    `Comment` VARCHAR(255),
+    `Associated` varchar(45) not null,
+	primary key (`Associated`, `StartDate`),
+	foreign key (`Associated`) references Associated(`ID`),
+	foreign key (`Author`) references Employee(`ID`)
+);
+
+CREATE TABLE `Activities`(
+	`id_act` INT AUTO_INCREMENT NOT NULL,
+	`owner` VARCHAR(45) NOT NULL,
+	`name` VARCHAR(45) NOT NULL, 
+	`duration` INT(3) NOT NULL,
+	`startDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	`endDate` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY(`id_act`)
+);
+
+CREATE TABLE `Owners_activities`(
+	`emp_id` INT NOT NULL,
+	`act_id` INT NOT NULL,
+	FOREIGN KEY(emp_id) REFERENCES Employee(`ID`),
+	FOREIGN KEY(act_id) REFERENCES Activities(`id_act`),
+	UNIQUE(emp_id, act_id)
+);
+
+
