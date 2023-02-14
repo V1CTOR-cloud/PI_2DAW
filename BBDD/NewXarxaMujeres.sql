73569348e91ef1e3b57fd75b502f48719e273f2a
Jococa01 <jococa01@floridauniversitaria.es>
2023-02-14 09:34:11 +0100

    Updating tables in DDBB

--- BBDD/NewXarxaMujeres.sql
+++ BBDD/NewXarxaMujeres.sql
@@ -21,20 +21,12 @@ create table `Associated`(
     );
 
     create table `Employee`(
+    `Email` varchar(45) not null,
 	`Name` varchar(45) not null,
     `Password` varchar(45) not null,
-    `Address` varchar(45) not null,
-    `BirthDate` datetime not null,
     `Profile/Position` varchar(45) not null,
     `Photo` mediumblob not null,
-    `DL` varchar(45) not null,
-    `PersonalVehicle` varchar(45) not null,
-    `SelfEmployedDisplacement` boolean not null,
-    `Adaptations` mediumblob not null,
-    `Loc`varchar(45),
-    `Prov`varchar(45),
-    `ID`varchar(45) not null,
-	primary key(`ID`)
+	primary key(`Email`)
 );
 
 create table `Session`(
@@ -47,7 +39,7 @@ create table `Session`(
     `Associated` varchar(45) not null,
 	primary key (`Associated`, `StartDate`),
 	foreign key (`Associated`) references Associated(`ID`),
-	foreign key (`Author`) references Employee(`ID`)
+	foreign key (`Author`) references Employee(`Email`)
 );
 
 CREATE TABLE `Activities`(
@@ -57,16 +49,24 @@ CREATE TABLE `Activities`(
 	`duration` INT(3) NOT NULL,
 	`startDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	`endDate` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-   FOREIGN KEY(`owner`) REFERENCES Employee(`ID`),
+	FOREIGN KEY(`owner`) REFERENCES Employee(`ID`),
 	PRIMARY KEY(`id_act`)
 );
 
-CREATE TABLE `Owners_activities`(
+CREATE TABLE `Members_activities`(
 	`Associated` VARCHAR(45) NOT NULL,
-	`act_id` INT NOT NULL,
+	`activity` INT NOT NULL,
 	FOREIGN KEY(`Associated`) REFERENCES Associated(`ID`),
-	FOREIGN KEY(act_id) REFERENCES Activities(`id_act`),
-	UNIQUE(`Associated`, act_id)
+	FOREIGN KEY(`activity`) REFERENCES Activities(`id_act`),
+	UNIQUE(`Associated`, `activity`)
+);
+
+CREATE TABLE `Employees_activities`(
+	`Employee` VARCHAR(45) NOT NULL,
+	`activity` INT NOT NULL,
+	FOREIGN KEY(`Employee`) REFERENCES Employee(`Email`),
+	FOREIGN KEY(`activity`) REFERENCES Activities(`id_act`),
+	UNIQUE(`Employee`, `activity`)
 );
 
 
