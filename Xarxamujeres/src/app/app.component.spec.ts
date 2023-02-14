56a105c399479d3df2eecfdd9055ffa89d2c0535
V1CTOR-cloud <latipicacuenta89@gmail.com>
2023-02-14 20:02:50 +0100

    Create angular skeleton structure
    Co-authored-by: AlcosaC <AlcosaC@users.noreply.github.com>

--- Xarxamujeres/src/app/app.component.spec.ts
+++ Xarxamujeres/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Xarxamujeres'`, () => {
+  it(`should have as title 'XarxaMujeres'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Xarxamujeres');
+    expect(app.title).toEqual('XarxaMujeres');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Xarxamujeres app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('XarxaMujeres app is running!');
   });
 });
--- Xarxamujeres/src/app/app.module.ts
+++ Xarxamujeres/src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ContentComponent } from './components/content/content.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
