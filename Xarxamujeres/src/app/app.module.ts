370d8a851cf3b7790eaaf38b55ee593b341eb491
V1CTOR-cloud <latipicacuenta89@gmail.com>
2023-02-15 00:22:36 +0100

    Creating login view, layouts and more stuff
    Co-authored-by: AlcosaC <AlcosaC@users.noreply.github.com>

--- Xarxamujeres/src/app/app.module.ts
+++ Xarxamujeres/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Views/login/login.component';
+import { ButtonComponent } from './components/button/button.component';
+import { SignUpComponent } from './Views/sign-up/sign-up.component';
+import { InputComponent } from './components/input/input.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ButtonComponent,
+    SignUpComponent,
+    InputComponent,
+    CarrouselComponent,
+    
   ],
   imports: [
     BrowserModule,
