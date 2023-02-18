370f93852652843bc401e0e7ddd3d2f166919296
AlcosaC <alcosa@floridauniversitaria.es>
2023-02-18 22:37:49 +0100

    Change on activities cards

--- Xarxamujeres/src/app/components/activities-card/activities-card.component.ts
+++ Xarxamujeres/src/app/components/activities-card/activities-card.component.ts
@@ -9,15 +9,17 @@ export class ActivitiesCardComponent {
   
   @Input() type: string = '';
   @Input() emoji: string ='🎉';
-  @Input() emoji2: string ='🎉';
-  @Input() emoji3: string ='🎉';
+  @Input() emoji2: string ='🏡';
+  @Input() emoji3: string ='⚙️';
   @Input() title: string = 'Valencian corts proposals';
-  @Input() title2: string = 'Valencian corts proposals';
-  @Input() title3: string = 'Valencian corts proposals';
+  @Input() title2: string = 'Marlene session';
+  @Input() title3: string = 'Memory Schedule';
   @Input() description: string = 'Today, 17 January 2022 was an important day for our association.';
   @Input() description2: string = 'Today, 17 January 2022 was an important day for our association.';
   @Input() description3: string = 'Today, 17 January 2022 was an important day for our association.';
   @Input() memberImage :string = "https://images.pexels.com/photos/4177563/pexels-photo-4177563.jpeg?auto=compress&cs=tinysrgb&w=600";
+  @Input() memberImage2 :string = "https://images.pexels.com/photos/3732652/pexels-photo-3732652.jpeg?auto=compress&cs=tinysrgb&w=600";
+  @Input() memberImage3 :string = "https://images.pexels.com/photos/13947143/pexels-photo-13947143.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load";
   
   
   
