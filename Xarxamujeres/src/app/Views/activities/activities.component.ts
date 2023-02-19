1ea468ca96a38cdaa95f37e54db1f8a640ca81b2
V1CTOR-cloud <latipicacuenta89@gmail.com>
2023-02-19 17:59:40 +0100

    re fixing layout in details
    Co-authored-by: AlcosaC <AlcosaC@users.noreply.github.com>

--- Xarxamujeres/src/app/Views/activities/activities.component.ts
+++ Xarxamujeres/src/app/Views/activities/activities.component.ts
@@ -10,8 +10,6 @@ export class ActivitiesComponent {
   public cardActivity : string = 'cardActivity';
   public activitiesLength: number = 54;
 
-  public emojiCard:string = '🎉';
-
   public col1:string = 'Events';
   public col2:string = 'Individual Session';
   public col3:string = 'Workshops';
